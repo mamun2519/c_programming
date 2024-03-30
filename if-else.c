9aa8b75b4e48d074688594822c3209f17365b20b
mamun2519 <juborajislam46@gmail.com>
2024-03-30 23:16:19 +0600

    loop relate problem

---
+++ if-else.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+      int n;
+      printf("Enter Number--");
+      scanf("%d", &n);
+      if (n % 2 == 0)
+      {
+            printf("This Is Even Number");
+      }
+      else
+      {
+            printf("This Is odd Number");
+      }
+      return 0;
+}
